46304c651207538227247e2f9eaad736c149cce9
Wpnnt <93552279+Wpnnt@users.noreply.github.com>
2022-04-06 15:45:14 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var name = prompt("What is Your Name ?");
+var firstChar = name.slice(0, 1);
+var upperCaseFirstChar = firstChar.toUpperCase();
+var restOfName = name.slice(1, name.length);
+var capitalisedName = upperCaseFirstChar + restOfName;
+alert(" hello " + capitalisedName);
